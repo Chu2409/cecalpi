1795ac4a5be14dde72fe8b3fda46df17d8df118d
Chu <dzhu2409@gmail.com>
2024-11-24 11:45:08 -0500

    Add netlify plugin and update dependencies

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,16 +1,18 @@
 const services = [
-    {
-        title: "Curso de formación en latonería automotriz",
-        description: "Domina las técnicas de reparación estructural y estética de carrocerías. Aprende procesos de desabollado, soldadura y más con equipos de última tecnología.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+  {
+    title: "Curso de formación en latonería automotriz",
+    description:
+      "Domina las técnicas de reparación estructural y estética de carrocerías. Aprende procesos de desabollado, soldadura y más con equipos de última tecnología.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
-    },
-    {
-        title: "Curso de formación en pintura automotriz",
-        description: "Perfecciona tus habilidades en aplicación de pintura, desde preparación de superficies hasta acabados profesionales. Garantizamos formación práctica y certificación reconocida.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+  },
+  {
+    title: "Curso de formación en pintura automotriz",
+    description:
+      "Perfecciona tus habilidades en aplicación de pintura, desde preparación de superficies hasta acabados profesionales. Garantizamos formación práctica y certificación reconocida.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
-    },
+  },
 ];
 
 export { services };
